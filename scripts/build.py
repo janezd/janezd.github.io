27ac9889376b5c03ed4bbe92740a10d5fd6c6057
janezd <janez.demsar@fri.uni-lj.si>
2015-04-15 18:37:48 +0200

    Refactor building into a separate function

--- scripts/build.py
+++ scripts/build.py
@@ -22,6 +22,42 @@ def insert_section(template, id, heading, content):
 		content = "<h2>{}</h2>{}".format(heading, content)
 	return template.replace("{{{{{}}}}}".format(id), content)
 
+def build_page(dir):
+    outdir = os.path.join(base_out, dir)
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    md = markdown.Markdown(extensions=["markdown.extensions.meta"])
+    main_html = md.convert(open(os.path.join(dir, "main.txt")).read())
+    html = tpl.replace("{{main}}", main_html)
+
+    meta = defaultdict(str)
+    meta.update(md.Meta)
+
+    thumb = os.path.join(dir, "thumbnail.png")
+    if os.path.exists(thumb):
+        shutil.copyfile(thumb, os.path.join(outdir, "thumbnail.png"))
+        thumb = '<img id="thumb" src="thumbnail.png"/>'
+    else:
+        thumb = ''
+    html = html.replace("{{thumbnail}}", thumb)
+
+    if "trajanje" in md.Meta:
+        duration = "<b>Trajanje</b>: " + md.Meta.get("trajanje")[0]
+    else:
+        duration = ""
+    html = html.replace("{{duration}}", duration)
+
+    for id, heading, meta in (
+            ("plan", "Potek učne ure", "priprava"),
+            ("material", "Učni listi in druge datoteke", "datoteke"),
+            ("resources", "Viri", "viri"), ("source", "Vir", "vir"),
+            ("video", "Video posnetki", "video"),
+            ("links", "Povezave", "povezave")):
+        html = insert_section(html, id, heading,
+                              parse_section(md.Meta.get(meta), dir, outdir))
+
+    open(os.path.join(outdir, "main.html"), "wt").write(html)
+
 
 tpl = open("page.tpl").read()
 os.chdir("..")
@@ -33,48 +69,6 @@ if not os.path.exists(base_out):
 shutil.copyfile("vidra.css", os.path.join(base_out, "vidra.css"))
 
 for dir in os.listdir("."):
-    main = os.path.join(dir, "main.txt")
-    if not os.path.exists(main):
-        continue
+    if os.path.exists(os.path.join(dir, "main.txt")):
+        build_page(dir)
 
-    outdir = os.path.join(base_out, dir)
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    md = markdown.Markdown(extensions=["markdown.extensions.meta"])
-    main_html = md.convert(open(main).read())
-    html = tpl.replace("{{main}}", main_html)
-
-    meta = defaultdict(str)
-    meta.update(md.Meta)
-
-    thumb = os.path.join(dir, "thumbnail.png")
-    if os.path.exists(thumb):
-        shutil.copyfile(thumb, os.path.join(outdir, "thumbnail.png"))
-        html = html.replace("{{thumbnail}}",
-                            '<img id="thumb" src="thumbnail.png"/>')
-    else:
-        html = html.replace("{{thumbnail}}", '')
-    html = insert_section(html, "plan", "Potek učne ure",
-                          parse_section(md.Meta.get("priprava"), dir, outdir))
-    if "trajanje" in md.Meta:
-        duration = "<b>Trajanje</b>: " + md.Meta.get("trajanje")[0]
-    else:
-        duration = ""
-    html = html.replace("{{duration}}", duration)
-
-    html = insert_section(html, "material", "Učni listi in druge datoteke",
-    	                  parse_section(md.Meta.get("datoteke"), dir, outdir))
-
-    html = insert_section(html, "resources", "Viri",
-                          parse_section(md.Meta.get("viri"), dir, outdir))
-
-    html = insert_section(html, "source", "Vir",
-                          parse_section(md.Meta.get("vir"), dir, outdir))
-
-    html = insert_section(html, "video", "Video posnetki",
-                          parse_section(md.Meta.get("video"), dir, outdir))
-
-    html = insert_section(html, "links", "Povezave",
-                          parse_section(md.Meta.get("povezave"), dir, outdir))
-
-    open(os.path.join(outdir, "main.html"), "wt").write(html)
